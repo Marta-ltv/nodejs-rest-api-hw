1291ade61f55bbc9dbcda133a5beadfc8e0b7769
Marta Litvinchuk <103766196+Marta-ltv@users.noreply.github.com>
2023-04-26 21:05:41 +0300

    Initial commit

---
+++ controllers/contacts.js
@@ -0,0 +1,69 @@
+const contacts = require("../models/contacts");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await contacts.getContactById(contactId);
+  if (!contact) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({
+    status: 'success',
+    code: 200,
+    data: { result: contact },
+  });
+};
+
+const addContact = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json({
+    status: 'success',
+    code: 201,
+    data: { result },
+  });
+};
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Contact deleted",
+  });
+};
+
+const upDateById = async (req, res) => {
+  const { contactId } = req.params;
+  if (Object.keys(req.body).length === 0) {
+    throw HttpError(400, 'missing fields');
+  }
+ 
+  const result = await contacts.updateById(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    status: 'success',
+    code: 201,
+    data: { result },
+  });
+};
+
+
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  upDateById: ctrlWrapper(upDateById),
+};
---
+++ routes/api/contacts.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody } = require("../../middlewares");
+
+const {putSchema, postSchema} = require("../../schemas/contacts");
+
+router.get("/", ctrl.getAll);
+
+router.get("/:contactId", ctrl.getContactById);
+
+router.post("/", validateBody(postSchema), ctrl.addContact);
+
+router.delete("/:contactId", ctrl.removeContact);
+
+router.put("/:contactId", validateBody(putSchema), ctrl.upDateById);
+
+
+module.exports = router;
