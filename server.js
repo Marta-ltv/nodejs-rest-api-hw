51a8838ac7493eac5ea9ecdfa86c30c1bc8c4a11
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-04-30 18:58:53 +0300

    add file env

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const app = require('./app');
 
-const DB_HOST =
-  "mongodb+srv://Martina:QZsC7odZNnlYOYDA@cluster0.fcknohc.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
+
 
 mongoose.set('strictQuery', true);
 
